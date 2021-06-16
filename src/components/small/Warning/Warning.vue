105eefc6302ca5dd066a3980d36c283b607103aa
chien.lo <chien.lo@staff.nueip.com>
2021-06-16 10:46:03 +0800

    [dev] 新增 warning

---
+++ src/components/small/Warning/Warning.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hrm-warning">
+      <slot>這是一個提示</slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HrmWarning'
+}
+</script>
+
+<style lang="scss" scoped>
+.hrm-warning{
+  border: solid 2px black;
+  border-radius: 5px;
+  background-color: rgba(#666, 0.2);
+  padding: 10px 18px;
+}
+</style>
